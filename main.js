b73353da509ee97048c9593308ff1f0b8e6c91c3
Ho Chi An <hochian31@gmail.com>
2023-07-12 22:55:14 +0700

    change form

---
+++ main.js
@@ -0,0 +1,34 @@
+var inforApi = 'http://localhost:3000/infor'
+
+
+function createInfor(data) {
+    var option = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    }
+    fetch(inforApi, option)
+        .then((reponse) => {
+            reponse.json()
+        })
+
+}
+handleCreateInfor();
+function handleCreateInfor() {
+    var buttonSend = document.getElementById("send-infor")
+    buttonSend.onclick = () => {
+        var nameInfor = document.querySelector('input[name="name-infor"]').value;
+        var emailInfor = document.querySelector('input[name="email-infor"]').value;
+        var messageInfor = document.querySelector('input[name="message-infor"]').value;
+        var dataInfor = {
+            name : nameInfor,
+            email: emailInfor,
+            message:messageInfor
+        }
+        createInfor(dataInfor)
+
+    }
+}
+
